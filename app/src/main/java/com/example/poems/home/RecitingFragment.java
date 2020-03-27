83388e8e58430e2ea9ec1e39da8887fb0ccb003c
Vicky <vicky@Vickys-MBP.local>
2020-03-27 18:21:35 +0800

    editing reciting fragment

--- app/src/main/java/com/example/poems/home/RecitingFragment.java
+++ app/src/main/java/com/example/poems/home/RecitingFragment.java
@@ -24,61 +24,65 @@ import com.example.poems.IdiomDetailActivity;
 import com.example.poems.IdiomListActivity;
 import com.example.poems.R;
 
+import java.util.List;
+
 import static java.lang.System.out;
 
 public class RecitingFragment extends Fragment {
     private Cursor cursor;
     private SQLiteDatabase db;
+    private int poemId;
+    private List<Integer> recitingIdList;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_reciting, container, false);
     }
 
-//    @Override
-//    public void onActivityCreated(Bundle savedInstanceState) {
-//        super.onActivityCreated(savedInstanceState);
-//        ListView listView = getView().findViewById(R.id.reciting_list);
-//
-//
-//        try {
-//            SQLiteOpenHelper poemDatabaseHelper = new DatabaseHelper(getContext());
-//            db = poemDatabaseHelper.getReadableDatabase();
-//            cursor = db.query("POEM",
-//                    new String[] {"_id", "TITLE", "POEM_ID"}, "grade = ?", new String[] {Integer.toString(grade)},
-//                    null, null, "_id");
-//            while (cursor.moveToNext()) {
-//                poemId = cursor.getInt(0);
-//                poemIdList.add(poemId);
-//            }
-//            CursorAdapter cursorAdapter = new SimpleCursorAdapter(this, R.layout.poem_item,
-//                    cursor, new String[]{"TITLE"}, new int[]{R.id.poem_name}, 0);
-////            gradeOneList.setAdapter(cursorAdapter);
-//            listView.setAdapter(cursorAdapter);
-//
-//        } catch(SQLiteException e) {
-//            System.out.print(e);
-//            Toast toast  = Toast.makeText(this, "Database unavailable", Toast.LENGTH_SHORT);
-//            toast.show();
-//
-//        }
-//
-//
-//
-//
-//        ArrayAdapter<Idiom> adapter = new ArrayAdapter<Idiom>(this.getContext(), android.R.layout.simple_list_item_1, idioms);
-////        ListView listView = getView().findViewById(R.id.idiom_list);
-//        listView.setAdapter(adapter);
-//
-//        AdapterView.OnItemClickListener itemClickListener = new AdapterView.OnItemClickListener() {
-//            public void onItemClick(AdapterView<?> listView, View itemView, int position, long id) {
-//                Intent intent = new Intent(getActivity(), IdiomDetailActivity.class);
-//                intent.putExtra(IdiomListActivity.IDIOM_ID, (int)id);
-//                startActivity(intent);
-//            }
-//        };
-//
-//        listView.setOnItemClickListener(itemClickListener);
-//
-//    }
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        ListView listView = getView().findViewById(R.id.reciting_list);
+
+
+        try {
+            SQLiteOpenHelper poemDatabaseHelper = new DatabaseHelper(getContext());
+            db = poemDatabaseHelper.getReadableDatabase();
+            cursor = db.query("POEM",
+                    new String[] {"_id", "TITLE", "POEM_ID"}, "is_reciting = ?", new String[] {Boolean.toString(true)},
+                    null, null, "_id");
+            while (cursor.moveToNext()) {
+                poemId = cursor.getInt(0);
+                poemIdList.add(poemId);
+            }
+            CursorAdapter cursorAdapter = new SimpleCursorAdapter(this, R.layout.poem_item,
+                    cursor, new String[]{"TITLE"}, new int[]{R.id.poem_name}, 0);
+//            gradeOneList.setAdapter(cursorAdapter);
+            listView.setAdapter(cursorAdapter);
+
+        } catch(SQLiteException e) {
+            System.out.print(e);
+            Toast toast  = Toast.makeText(this, "Database unavailable", Toast.LENGTH_SHORT);
+            toast.show();
+
+        }
+
+
+
+
+        ArrayAdapter<Idiom> adapter = new ArrayAdapter<Idiom>(this.getContext(), android.R.layout.simple_list_item_1, idioms);
+//        ListView listView = getView().findViewById(R.id.idiom_list);
+        listView.setAdapter(adapter);
+
+        AdapterView.OnItemClickListener itemClickListener = new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> listView, View itemView, int position, long id) {
+                Intent intent = new Intent(getActivity(), IdiomDetailActivity.class);
+                intent.putExtra(IdiomListActivity.IDIOM_ID, (int)id);
+                startActivity(intent);
+            }
+        };
+
+        listView.setOnItemClickListener(itemClickListener);
+
+    }
 }
